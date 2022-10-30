b7300ea25ad0e85c397951c5d3d86facc8fd20d0
Rahul Gohel <rahulgohel8317@gmail.com>
2022-10-30 18:39:28 +0530

    remove create indeX

--- src/database/conn.js
+++ src/database/conn.js
@@ -6,7 +6,7 @@ mongoose
   .connect(`mongodb+srv://rahulgohel:Rahulgohel123@cluster0.9m5lhzt.mongodb.net/database?retryWrites=true&w=majority`, {
     useUnifiedTopology:true,
     useNewUrlParser: true,
-    useCreateIndex: true
+   
   })
   .then(() => {
     console.log("connection successful");
