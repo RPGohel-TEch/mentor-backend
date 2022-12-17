1522649dd71805f29b0cdb6166203bd1b6f40b87
sahil <sahil1212d@gmail.com>
2022-12-17 11:24:46 +0530

    fixes

--- src/api/user/faculty/faculty.model.js
+++ src/api/user/faculty/faculty.model.js
@@ -3,11 +3,9 @@ const mongoose = require("mongoose");
 const facultySchema = mongoose.Schema({
   name: {
     type: String,
-    required: true,
   },
   email: {
     type: String,
-    required: true,
   },
   password: {
     type: String,
